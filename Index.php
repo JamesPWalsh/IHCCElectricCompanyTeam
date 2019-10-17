7bce89f8026438dc8b9ebcaad821cd48e5b3f41f
InvertDew <justinleeoglesby@gmail.com>
2019-10-17 10:30:42 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -16,7 +16,35 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php include 'loginchecks.php'?>
-<?php include 'navbar.html'?>
+<div id="navbar">
+<div class="login-container">
+      <a id='sign-out' href='login.php?signout=true' class='effect' style='display:none;'>Sign Out&nbsp;&#x2607;</a>
+    <form action="login.php" method="POST" id="loginform" style="display:none;">
+      <input class="login" type="text" placeholder="Username" name="username">
+      <input class="login" type="password" placeholder="Password" name="password">
+      <button type="submit" id="login">Login</button>
+    </form>
+</div>
+<div class="topnav" style="display:none">
+  <a href="javascript:void(0);" class="icon" id="icon" onclick="myFunction()" style="">
+  <div class="clearfix"></div><i class="fa fa-bars fa-2x"></i>
+  </a>
+
+<div id="myLinks">
+  <a href="About.php" id="droplink">About Us</a>
+<a href="FAQ.php" id="droplink">Faq Page</a>
+ <a href="modelorder.php" id="droplink">Order Here</a>
+  <a href="Contact.php" id="droplink">Contact Us</a>
+</div>
+</div>
+<ul class="effect" id="effect">
+<li><a href="About.php">About Us</a></li>
+<li><a href="FAQ.php">Faq Page</a></li>
+<li><a href="modelorder.php">Order Here</a></li>
+ <li><a href="Contact.php">Contact Us</a></li>
+</ul>
+</div>
+
 	<style>
   div#navbar {
     position:fixed;
