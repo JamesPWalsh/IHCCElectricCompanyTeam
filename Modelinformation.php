2b46c54fbf9ac1bb70a9d8ef21aa70e028755e55
EthanO <55151145+EthanO@users.noreply.github.com>
2019-10-29 00:42:55 -0700

    Update Modelinformation.php

--- Modelinformation.php
+++ Modelinformation.php
@@ -2,6 +2,10 @@
 <html>
 
 <head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
   <link rel="stylesheet" type="text/css" media="screen" href="navbar.css">
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -29,6 +33,50 @@ padding-bottom: 15px;
   background-color: #f2f2f2;
   padding: 20px;
 }
+
+.container1 {
+  width: 90%;
+  border-radius: 10px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+
+
+
+/* SKILLS 1 */
+
+.containera {
+  
+  width: 80%;
+  background-color:r;
+  margin-left:222px;
+}
+
+.imagea {
+  width: 90%;
+  height: 90%;
+  padding:1px;
+}
+
+.overlaya {
+  position: absolute;
+   padding: 20px;
+   border-radius: 10px;
+
+  top: 5%;
+  right:0% ;
+  left: 20%;
+  height: 50%;
+  width: 80%;
+  opacity: 0;
+  
+  transition: .1s ease;
+}
+
+.containera:hover .overlaya {
+  opacity: 2; 
+}
 </style>
 
 <!--Administration.html
@@ -37,63 +85,96 @@ padding-bottom: 15px;
 	<meta charset="utf-8">
 </head>
 <body>
-<div class="container">
-<center>
-<h1>Sparkle (48v) information</h1>
-</center>
-<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
-<ul>
-<li>Morbi accumsan enim vel aliquam placerat</li>
-<li>Proin id ultrices enim</li>
-<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
-<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
-</ul>
-<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
+<div class ="container">
 
-Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
-</p>
-<center>
-<h1>Lightning (96v) information</h1>
-</center>
-<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
-<ul>
-<li>Morbi accumsan enim vel aliquam placerat</li>
-<li>Proin id ultrices enim</li>
-<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
-<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
-</ul>
-<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
+<div class="containera">
+		<center>
+		<img width="800" height="400" <img src="images/Sparkle.png">
+		</center>
+		<center>
+		<img width="800" height="400" <img src="images/Lightning.png">
+		</center>
+		<center>
+		<img width="800" height="400" <img src="images/Thunder.png">
+		</center>
+		<center>
+		<img width="800" height="400" <img src="images/FireFly.png">
+		</center>
+	<div class="overlaya">
+				
+				
+				<div class="container1">
+						<center>
+						<h1>Sparkle (48v) information</h1>
+						</center>
+						<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
+						<ul>
+						<li>Morbi accumsan enim vel aliquam placerat</li>
+						<li>Proin id ultrices enim</li>
+						<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
+						<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
+						</ul>
+						<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
 
-Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
-</p>
-<center>
-<h1>Thunder (192v) information</h1>
-</center>
-<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
-<ul>
-<li>Morbi accumsan enim vel aliquam placerat</li>
-<li>Proin id ultrices enim</li>
-<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
-<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
-</ul>
-<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
+						Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
+						</p>
+						
+						<center>
+							<h1>Lightning (96v) information</h1>
+							</center>
+						<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
+						<ul>
+						<li>Morbi accumsan enim vel aliquam placerat</li>
+						<li>Proin id ultrices enim</li>
+						<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
+						<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
+						</ul>
+						<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
 
-Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
-</p>
-<center>
-<h1>Firefly (24v) information</h1>
-</center>
-<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
-<ul>
-<li>Morbi accumsan enim vel aliquam placerat</li>
-<li>Proin id ultrices enim</li>
-<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
-<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
-</ul>
-<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
+						Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
+						</p>
+												
+												
+						<center>
+						<h1>Thunder (192v) information</h1>
+						</center>
+						<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
+						<ul>
+						<li>Morbi accumsan enim vel aliquam placerat</li>
+						<li>Proin id ultrices enim</li>
+						<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
+						<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
+						</ul>
+						<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
 
-Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
-</p>
+						Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
+						</p>
+						<center>
+						<h1>Firefly (24v) information</h1>
+						</center>
+						<p>Integer imperdiet tristique luctus. Aenean a tempor quam. Vivamus tristique porta ex, eu viverra massa sodales faucibus. Morbi accumsan enim vel aliquam placerat. Vivamus fringilla tristique nunc, sit amet faucibus ante auctor et. Proin id ultrices enim. Proin lacus nisl, consequat et porta vel, dapibus nec felis. Nulla in dui sit amet dui feugiat ullamcorper ac non dolor. Quisque in dui posuere magna mollis porttitor sed ut leo. Cras blandit finibus turpis et scelerisque.</p>
+						<ul>
+						<li>Morbi accumsan enim vel aliquam placerat</li>
+						<li>Proin id ultrices enim</li>
+						<li>Proin lacus nisl, consequat et porta vel, dapibus nec felist</li>
+						<li>Quisque in dui posuere magna mollis porttitor sed ut leo</li>
+						</ul>
+						<p>Aenean eu mi a est aliquam congue. Vestibulum sodales libero id mi egestas porttitor. Pellentesque aliquam velit in venenatis dictum. Donec ultricies ut orci ut eleifend. Fusce consequat mollis commodo. Vivamus sit amet facilisis orci. Duis tristique justo et neque malesuada, id dignissim nulla condimentum. Integer id vulputate est, euismod viverra justo. Nullam blandit nisl rutrum commodo aliquet. In hac habitasse platea dictumst. Proin gravida neque accumsan nunc consequat accumsan. Mauris eu lectus nec risus rhoncus congue id id magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget nulla dapibus, egestas odio eu, dapibus lorem. Mauris egestas maximus eros, vel sollicitudin odio. Integer ultrices libero sed vestibulum placerat.
+
+						Nulla mollis mi in nunc aliquet, et aliquam urna malesuada. Morbi sed interdum augue. Maecenas augue turpis, ultricies et ultricies nec, pretium non magna. Fusce aliquam ante mauris, ut imperdiet sem blandit ac. In feugiat lectus molestie tortor pulvinar, vitae mattis ex fermentum. Fusce non libero quis augue maximus tincidunt sed non ante. Sed vel quam purus. Quisque euismod faucibus velit a ullamcorper. Nunc sem augue, gravida sed lacinia eget, aliquam ut libero. Cras tristique finibus commodo. Quisque nec aliquam urna.
+						</p>
+
+				</div>
+				</div>
+				</div>
+				
+				
+			
+				
+
+
+
+
 </div>
 	</body>
   <script>
